e0e19c81fe46ddb6d0ebb5d540867bf45e1ccbed
Darshit <darshitmistry2@gmail.com>
2023-03-20 18:46:27 +0530

    The Order Page wasn added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({catagory,title}) => {
   return (
-    <div>
-      header
+    <div className='mb-10'>
+      <p className="text-gray-400">
+        {catagory}
+      </p>
+      <p className="text-3xl font-extrabold tracking-tight text-slate-900">{title}</p>
     </div>
   )
 }
--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Order = () => {
   return (
-    <div>
-      Order
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-full">
+      <Header category="page" title="Orders" />
+      <GridComponent id="gridcomp" dataSource={ordersData} allowPaging allowSorting>
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Resize,Sort,ContextMenu,Filter,Page,ExcelExport,Edit,PdfExport]}/>
+      </GridComponent>
     </div>
-  )
-}
+  );
+};
 
-export default Order
+export default Order;
