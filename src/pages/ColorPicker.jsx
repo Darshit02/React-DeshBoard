cea0824358011a812b88917b93f9ba591c737171
Darshit <darshitmistry2@gmail.com>
2023-03-22 18:38:22 +0530

    A colorPicker was added

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import { ColorPickerComponent } from "@syncfusion/ej2-react-inputs";
+import { Header } from "../components";
 
+
+const change = (args) =>{
+  document.getElementById('preview').style.backgroundColor = args.currentValue.hex;
+}
 const ColorPicker = () => {
   return (
-    <div>
-      ColorPicker
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header catagory="App" title="Color Picker" />
+      <div className="text-center">
+        <div id="preview" />
+        <div className="flex justify-center items-center gap-20 flex-wrap">
+          <div>
+            <p className="text-2xl font-semibold mt-2 mb-4">Inline Pallate</p>
+            <ColorPickerComponent
+              id="inline-pallete"
+              mode="Palette"
+              modeSwitcher={false}
+              inline
+              showButtons={false}
+              change={change}
+            />
+            <div>
+              <p className="text-2xl font-semibold mt-2 mb-4">Inline Pallate</p>
+              <ColorPickerComponent
+                id="inline-pallete"
+                mode="Picker"
+                modeSwitcher={false}
+                inline
+                showButtons={false}
+                change={change}
+                />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ColorPicker
+export default ColorPicker;
