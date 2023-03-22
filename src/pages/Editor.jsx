6b60dad730b2e10637c0678f983227edc023ec4c
Darshit <darshitmistry2@gmail.com>
2023-03-22 18:22:04 +0530

    A Editor was added

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
+import { EditorData } from "../data/dummy";
+import { Header } from "../components";
 
 const Editor = () => {
   return (
-    <div>
-      Edit
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header catagory="App" title="Editor" />
+      <RichTextEditorComponent>
+        <Inject services={[HtmlEditor, Toolbar, Link, QuickToolbar, Image]} />
+      </RichTextEditorComponent>
     </div>
-  )
-}
+  );
+};
 
-export default Editor
+export default Editor;
